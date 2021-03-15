27a26e505de3b98d42b5f0933d18073b8304d509
Thomas Stubbs <46704433+thomas-stubbs@users.noreply.github.com>
2021-03-14 18:35:00 -0700

    Update and rename PWM.h to InputCapture.h

---
+++ code/InputCapture.h
@@ -0,0 +1,8 @@
+#ifndef __STM32L476G_DISCOVERY_INPUT_CAPTURE_H
+#define __STM32L476G_DISCOVERY_INPUT_CAPTURE_H
+
+#include "stm32l476xx.h"
+
+void Input_Capture_Init(void);
+
+#endif /* __STM32L476G_DISCOVERY_INPUT_CAPTURE_H */
