bbc7f27336e88cfeb47aa9a2be8d88d674f1e657
Thomas Stubbs <46704433+thomas-stubbs@users.noreply.github.com>
2021-03-15 10:26:54 -0700

    move interrupt handlers to main.c

--- code/main.c
+++ code/main.c
@@ -6,6 +6,61 @@
 #include "SysClock.h"
 #include "stm32l476xx.h"
 
+int volatile page = 0;
+uint32_t volatile speed_last_value = 0;
+uint32_t volatile cadence_last_value = 0;
+uint32_t volatile speed_current_value = 0;
+uint32_t volatile cadence_current_value = 0;
+uint32_t volatile speed_overflow_count = 0;
+uint32_t volatile cadence_overflow_count = 0;
+uint32_t volatile speed_time_interval = 0;
+uint32_t volatile cadence_time_interval = 0;
+
+void EXTI1_IRQHandler(void) {
+	EXTI->PR1 |= EXTI_PR1_PIF1;
+	if(!page)
+		page = 2;
+	else
+		page--;
+}
+
+void EXTI2_IRQHandler(void) {
+	EXTI->PR1 |= EXTI_PR1_PIF2;
+	if(page == 2)
+		page = 0;
+	else
+		page++;
+}
+
+void TIM1_IRQHandler(void) {
+	if(TIM4->SR & TIM_SR_UIF){
+		TIM4->SR &= ~TIM_SR_UIF;
+		speed_overflow_count++;
+		cadence_overflow_count++;
+	}if(TIM4->SR & TIM_SR_CC3IF){ //Wheel Magent
+		TIM4->SR &= ~TIM_SR_CC3IF;
+		if(!speed_last_value)
+			speed_last_value = TIM1->CCR3;
+		else{
+			speed_current_value = TIM1->CCR3;
+			speed_time_interval = (0x10000*speed_overflow_count)+(speed_current_value-speed_last_value);
+			speed_overflow_count = 0;
+			speed_last_value = speed_current_value;
+		}
+	}
+	if(TIM4->SR & TIM_SR_CC4IF){ //Crank Magnet
+		TIM4->SR &= ~TIM_SR_CC4IF;
+		if(!cadence_last_value)
+			cadnece_last_value = TIM1->CCR4;
+		else{
+			cadence_current_value = TIM1->CCR4;
+			cadence_time_interval = (0x10000*cadence_overflow_count)+(cadence_current_value-cadence_last_value);
+			cadence_overflow_count = 0;
+			cadence_last_value = cadence_current_value;
+		}
+	}
+}
+
 int main(void){
 	System_Clock_Init(); // System Clock = 80 MHz
 
@@ -39,4 +94,3 @@ int main(void){
 		}
 	}
 }
-
